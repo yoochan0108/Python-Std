60f5036ce84648d40220e0c540b2a0e4c22281a0
yoochan0108 <yoochan0108@naver.com>
2024-12-22 12:39:45 +0900

    default Parameters

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-#function
-def say_hello():
-  print("hello how r u?")
+def say_hello(user_name="anonymous"):
+  print("hello", user_name)
 
+say_hello("nico")
 say_hello()
+
+#parameater에 기본값 출력방법
+#함수="기본값이름"
