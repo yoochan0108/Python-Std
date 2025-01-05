08b5bb30be97a555644bd3b4a187abfde51415bc
yoochan0108 <yoochan0108@naver.com>
2025-01-05 15:56:51 +0900

    return값 활용

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-my_name = "nico"
-my_age = 12
-my_color_eyes = "brown"
+def make_juice(fruit):
+  return f"{fruit}+🥤"
 
-print(f"Hello I'm {my_name}, I have {my_age} years in the earth, {my_color_eyes} is my eye color")
+def add_ice(juice):
+  return f"{juice}+🧊"
+
+def add_sugar(iced_juice):
+  return f"{iced_juice}+🍬"
+
+juice = make_juice("🍎")
+cold_juice = add_ice(juice)
+perfect_juice = add_sugar(cold_juice)
+
+print(perfect_juice)
